193df6f5465a48b301b9bbd85650320721a14382
JJRR <34721896+juanjo097@users.noreply.github.com>
2021-03-16 23:28:11 -0600

    primer commit con la primera version funcional

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let theme_lclstr = localStorage.getItem('theme');
+theme_lclstr ==  null ? setTheme('light') : setTheme(theme_lclstr);
+
+let theme_dots = document.getElementsByClassName('theme-dot');
+
+
+for (var i = 0; theme_dots.length > i; i++) {
+
+    theme_dots[i].addEventListener('click', function()
+    {
+        let mode = this.dataset.mode;
+        console.log('clicked color: ', mode);
+        setTheme(mode);
+    })
+    
+}
+
+function setTheme(mode)
+{
+    switch (mode) 
+    {
+        case 'light':
+            document.getElementById('theme-style').href = 'styles.css';
+            break;
+        case 'blue':
+            document.getElementById('theme-style').href = 'blue.css';
+            break;
+        case 'green':
+            document.getElementById('theme-style').href = 'green.css';
+            break;
+        case 'purple':
+            document.getElementById('theme-style').href = 'purple.css';
+            break;
+        default:
+            document.getElementById('theme-style').href = 'styles.css';
+            break;
+    }
+    localStorage.setItem('theme', mode)
+}
